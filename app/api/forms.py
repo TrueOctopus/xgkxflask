430bacdc26a23659017322b6672b580660eb20f7
trueoctopus <18368876370@163.com>
2020-09-12 19:52:00 +0800

    更新一波后台管理api

--- app/api/forms.py
+++ app/api/forms.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, SubmitField
-from wtforms.validators import DataRequired, EqualTo
+from wtforms.validators import DataRequired, EqualTo, length
 
 
 class ForgetPwdForm(FlaskForm):
     password = PasswordField('密码', validators=[DataRequired(),
                                                EqualTo(
                                                    'password2',
-                                                   message='两次密码必须相同')])
-    password2 = PasswordField('确认密码', validators=[DataRequired()])
+                                                   message='两次密码必须相同'),
+                                               length(min=6, max=30,
+                                                      message='长度必须为6位以上')])
+    password2 = PasswordField('确认密码', validators=[DataRequired(),
+                                                  length(min=6, max=30,
+                                                         message='长度必须为6位以上')])
     submit = SubmitField('更改密码')
